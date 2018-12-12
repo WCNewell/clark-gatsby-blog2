24c1e1b2c10a17db2e31fdff817d6008f804b71a
Clark Newell <williamcnewell@gmail.com>
2018-12-11 17:40:57 -0700

    completed gatsby-node & postLayout with PostQuery

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,36 @@
+const path = require('path')
 
+exports.createPages = ({ graphql, actions }) => {
+    const { createPage } = actions
+    return new Promise((resolve, reject) => {
+        graphql(`
+            {
+                allMarkdownRemark {
+                    edges {
+                        node {
+                            frontmatter {
+                                title
+                                slug
+                            }
+                        }
+                    }
+                }
+            }
+        `).then(results => {
+            results.data.allMarkdownRemark.edges.forEach(({ node }) => {
+                createPage({
+                    path: `/posts${node.frontmatter.slug}`,
+                    component: path.resolve('./src/components/postLayout.js'),
+                    context: {
+                        slug: node.frontmatter.slug,
 
+                    }
+                })
+            })
+        })
+        resolve()
+    })
+}
 
 /**
  * Implement Gatsby's Node APIs in this file.
